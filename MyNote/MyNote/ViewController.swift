91fc37620ad75dd8e1e2a54564aeabc2b5f66396
Allicia Viona <alliciawn@gmail.com>
2022-04-20 09:02:49 +0700

    Project init

---
+++ MyNote/MyNote/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyNote
+//
+//  Created by Allicia Viona Sagi on 20/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
